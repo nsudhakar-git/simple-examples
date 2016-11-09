242dc71e72b6d507965826e763860c801b3064a5
Sudhakar Namasivayam <tsxn7@unigroupinc.com>
2016-11-09 12:23:20 -0600

    initial commit

---
+++ src/main/java/com/example/SimpleExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleExamplesApplication.class, args);
+	}
+}
